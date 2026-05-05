34e80636e80a30f1866708456e8753cfb80f0863
MOHAMMAD SHAMSUDDOHA SHAMS <153288639+shams909@users.noreply.github.com>
2026-05-05 14:10:05 +0600

    Add project files.

---
+++ Vanguard Engine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vanguard_Engine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
